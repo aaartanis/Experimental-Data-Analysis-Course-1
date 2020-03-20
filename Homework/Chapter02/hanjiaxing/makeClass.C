e56f2a9dd6c4458a9a23524a5f9e66391b502621
jiaxinghan <hanjiaxing0609@163.com>
2020-03-20 20:28:13 +0800

    f

--- Homework/Chapter02/hanjiaxing/makeClass.C
+++ Homework/Chapter02/hanjiaxing/makeClass.C
@@ -5,6 +5,11 @@
 #include <TCanvas.h>
 #include <TRandom3.h> 
 #include <TMath.h> 
+#include <iostream> 
+
+#if !defined(__CLING__) || defined(__ROOTCLING__) 
+ClassImp(makeClass);
+#endif
 
 void makeClass::Loop()
 {
@@ -61,6 +66,7 @@ void makeClass::Loop()
   TRandom3 *gr = new TRandom3(0);
 
   TFile *opf = new TFile("veto.root","recreate");
+  //TTree *opt = new TTree("tree","veto");
   TTree *opt = (TTree*)fChain->Clone(0);
 
   Double_t xv;
@@ -74,17 +80,17 @@ void makeClass::Loop()
   Int_t itdv;
   Int_t iquv;
   Int_t iqdv;
-  opt->Branch("xv",&xv,"ex/D");
-  opt->Branch("ev",&ev,"ev/D");
-  opt->Branch("tofv",&tofv,"tofv/D");
-  opt->Branch("tuv",&tuv,"tuv/D");
-  opt->Branch("tdv",&tdv,"tdv/D");
-  opt->Branch("quv",&quv,"quv/D");
-  opt->Branch("qdv",&qdv,"qdv/D");
-  opt->Branch("ituv",&ituv,"ituv/I");
-  opt->Branch("itdv",&itdv,"itdv/I");
-  opt->Branch("iquv",&iquv,"iquv/I");
-  opt->Branch("iqdv",&iqdv,"iqdv/I");
+  //opt->Branch("xv",&xv,"xv/D");
+  //opt->Branch("ev",&ev,"ev/D");
+  //opt->Branch("tofv",&tofv,"tofv/D");
+  //opt->Branch("tuv",&tuv,"tuv/D");
+  //opt->Branch("tdv",&tdv,"tdv/D");
+  //opt->Branch("quv",&quv,"quv/D");
+  //opt->Branch("qdv",&qdv,"qdv/D");
+  //opt->Branch("ituv",&ituv,"ituv/I");
+  //opt->Branch("itdv",&itdv,"itdv/I");
+  //opt->Branch("iquv",&iquv,"iquv/I");
+  //opt->Branch("iqdv",&iqdv,"iqdv/I");
 
   if (fChain == 0) return;
   Long64_t nentries = fChain->GetEntriesFast();
@@ -95,86 +101,89 @@ void makeClass::Loop()
     nb = fChain->GetEntry(jentry);   nbytes += nb;
     // if (Cut(ientry) < 0) continue;
 
+
     xv = x;
 
-    if(pid==2) {
-      ev = e*0.1;
-      e = e*0.9;
-      tofv = tof;
+    if(jentry%100000==0 || jentry==999999) std::cout << jentry << "/" << nentries << "    " << x << "    " << xv << std::endl;
 
-      tu = tof+(L-x)/Vsc+gr->Gaus(0,TRes/2.35)+tu_off;
-      td = tof+(L+x)/Vsc+gr->Gaus(0,TRes/2.35)+td_off;
-      tu *= TDCch2ns;
-      td *= TDCch2ns;
+    //if(pid==2) {
+    //  ev = e*0.1;
+    //  e = e*0.9;
+    //  tofv = tof;
 
-      tuv = tofv+(L-x)/Vsc+gr->Gaus(0,TRes/2.35)+tu_offv;
-      tdv = tofv+(L+x)/Vsc+gr->Gaus(0,TRes/2.35)+td_offv;
-      tuv *= TDCch2ns;
-      tdv *= TDCch2ns;
+    //  tu = tof+(L-x)/Vsc+gr->Gaus(0,TRes/2.35)+tu_off;
+    //  td = tof+(L+x)/Vsc+gr->Gaus(0,TRes/2.35)+td_off;
+    //  tu *= TDCch2ns;
+    //  td *= TDCch2ns;
 
-      Double_t q0 = e*ADCgain; //charged particle
-      //q0 = gr->Gaus(q0,q0*QRes/2.35);
-      qu = q0*TMath::Exp(-(L-x)/Lambda);
-      qd = q0*TMath::Exp(-(L+x)/Lambda);
-      qu = gr->Gaus(qu,qu*QRes/2.35);
-      qd = gr->Gaus(qd,qd*QRes/2.35);
-      
-      Double_t q0v = ev*ADCgainv;
-      //q0v = gr->Gaus(q0v,q0v*QRes/2.35);
-      quv = q0v*TMath::Exp(-(L-x)/Lambda);
-      qdv = q0v*TMath::Exp(-(L+x)/Lambda);
-      quv = gr->Gaus(quv,quv*QRes/2.35);
-      qdv = gr->Gaus(qdv,qdv*QRes/2.35);
+    //  tuv = tofv+(L-x)/Vsc+gr->Gaus(0,TRes/2.35)+tu_offv;
+    //  tdv = tofv+(L+x)/Vsc+gr->Gaus(0,TRes/2.35)+td_offv;
+    //  tuv *= TDCch2ns;
+    //  tdv *= TDCch2ns;
 
-      if(qu<100) tu = TDCoverflow;
-      if(qd<100) td = TDCoverflow;
-      if(quv<100) tuv = TDCoverflow;
-      if(qdv<100) tdv = TDCoverflow;
+    //  Double_t q0 = e*ADCgain; //charged particle
+    //  //q0 = gr->Gaus(q0,q0*QRes/2.35);
+    //  qu = q0*TMath::Exp(-(L-x)/Lambda);
+    //  qd = q0*TMath::Exp(-(L+x)/Lambda);
+    //  qu = gr->Gaus(qu,qu*QRes/2.35);
+    //  qd = gr->Gaus(qd,qd*QRes/2.35);
+    //  
+    //  Double_t q0v = ev*ADCgainv;
+    //  //q0v = gr->Gaus(q0v,q0v*QRes/2.35);
+    //  quv = q0v*TMath::Exp(-(L-x)/Lambda);
+    //  qdv = q0v*TMath::Exp(-(L+x)/Lambda);
+    //  quv = gr->Gaus(quv,quv*QRes/2.35);
+    //  qdv = gr->Gaus(qdv,qdv*QRes/2.35);
 
-      qu += gr->Gaus(ADCuPed,ADCnoise);
-      qd += gr->Gaus(ADCdPed,ADCnoise);
-      if(qu<0) qu=0;
-      if(qd<0) qd=0;
+    //  if(qu<100) tu = TDCoverflow;
+    //  if(qd<100) td = TDCoverflow;
+    //  if(quv<100) tuv = TDCoverflow;
+    //  if(qdv<100) tdv = TDCoverflow;
 
-      quv += gr->Gaus(ADCuPedv,ADCnoise);
-      qdv += gr->Gaus(ADCdPedv,ADCnoise);
-      if(quv<0) quv=0;
-      if(qdv<0) qdv=0;
-    }
-    else {
-      ev = 0;
-      tofv = -1;
-      tuv = TDCoverflow;
-      tdv = TDCoverflow;
-      quv = ADCuPedv + gr->Gaus(0,ADCnoise);
-      qdv = ADCdPedv + gr->Gaus(0,ADCnoise);
-    }
+    //  qu += gr->Gaus(ADCuPed,ADCnoise);
+    //  qd += gr->Gaus(ADCdPed,ADCnoise);
+    //  if(qu<0) qu=0;
+    //  if(qd<0) qd=0;
 
-    //overflow check
-    if(tu>TDCoverflow) tu=TDCoverflow;
-    if(td>TDCoverflow) td=TDCoverflow;
-    if(qu>ADCoverflow) qu=ADCoverflow;
-    if(qd>ADCoverflow) qd=ADCoverflow;
+    //  quv += gr->Gaus(ADCuPedv,ADCnoise);
+    //  qdv += gr->Gaus(ADCdPedv,ADCnoise);
+    //  if(quv<0) quv=0;
+    //  if(qdv<0) qdv=0;
+    //}
+    //else {
+    //  ev = 0;
+    //  tofv = -1;
+    //  tuv = TDCoverflow;
+    //  tdv = TDCoverflow;
+    //  quv = ADCuPedv + gr->Gaus(0,ADCnoise);
+    //  qdv = ADCdPedv + gr->Gaus(0,ADCnoise);
+    //}
 
-    if(tuv>TDCoverflow) tuv=TDCoverflow;
-    if(tdv>TDCoverflow) tdv=TDCoverflow;
-    if(quv>ADCoverflow) quv=ADCoverflow;
-    if(qdv>ADCoverflow) qdv=ADCoverflow;
+    ////overflow check
+    //if(tu>TDCoverflow) tu=TDCoverflow;
+    //if(td>TDCoverflow) td=TDCoverflow;
+    //if(qu>ADCoverflow) qu=ADCoverflow;
+    //if(qd>ADCoverflow) qd=ADCoverflow;
 
-    //digitization
-    itu = Int_t(tu);
-    itd = Int_t(td);
-    iqu = Int_t(qu);
-    iqd = Int_t(qd);
+    //if(tuv>TDCoverflow) tuv=TDCoverflow;
+    //if(tdv>TDCoverflow) tdv=TDCoverflow;
+    //if(quv>ADCoverflow) quv=ADCoverflow;
+    //if(qdv>ADCoverflow) qdv=ADCoverflow;
 
-    ituv = Int_t(tuv);
-    itdv = Int_t(tdv);
-    iquv = Int_t(quv);
-    iqdv = Int_t(qdv);
+    ////digitization
+    //itu = Int_t(tu);
+    //itd = Int_t(td);
+    //iqu = Int_t(qu);
+    //iqd = Int_t(qd);
+
+    //ituv = Int_t(tuv);
+    //itdv = Int_t(tdv);
+    //iquv = Int_t(quv);
+    //iqdv = Int_t(qdv);
 
     opt->Fill();
   }
 
-  opt->Write();
+  opf->Write();
   opf->Close();
 }
